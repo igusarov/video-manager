71521528ef326d2fe2730f272f10a4e646bdabbd
Ilya Gusarov <i.gusarov86@gmail.com>
2020-11-21 01:31:16 +0400

    implement get videos

--- src/services/videos.ts
+++ src/services/videos.ts
@@ -5,7 +5,22 @@ import { getAuthors } from './authors';
 export const getVideos = (): Promise<Video[]> => {
   return Promise.all([getCategories(), getAuthors()])
     .then(([categories, authors]) => {
-    // TODO: implement here
-    return [];
+    return authors.reduce<Video[]>((acc, author) => {
+
+      const videos = author.videos.map((video) => ({
+        id: video.id,
+        name: video.name,
+        author: author.name,
+        categories: video.catIds.map((catId) => {
+          const category =  categories.find((cat) => cat.id === catId);
+          return category ? category.name : '';
+        }).filter(Boolean)
+      }));
+
+      return [
+        ...acc,
+        ...videos,
+      ];
+    }, []);
   });
 };
